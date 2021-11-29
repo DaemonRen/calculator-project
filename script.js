d9a308d0a5d4819c94b47c699b54645bd15135c7
Ben Augood <ben_augood@outlook.com>
2021-11-29 15:18:04 +0000

    Test

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-
-
-
-
-// Define my constants
-const numBtn = document.querySelectorAll(".num__btn");
-// const opBtn = document.querySelectorAll(".op__btn");
-const output = document.querySelectorAll(".output");
-const prevOp = document.querySelectorAll(".prev__Op");
-const currentOp = document.querySelector("#current__Op");
-
-// const multiBtn = document.querySelector("#multi__btn");
-// const divideBtn = document.querySelector("#divide__btn");
-// const addBtn = document.querySelector("#add__btn");
-// const subBtn = document.querySelector("#sub__btn");
-
-// const decimalBtn = document.querySelector("#decimal__btn");
-const acBtn = document.querySelector("#ac__btn");
-const delBtn = document.querySelector("#del__btn");
-const eqBtn = document.querySelector("#eq__btn");
-
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "."];
-let sum = [];
-
-
 // 1. Have each num button attached to an event listener
 
 // 2. Have pre-determined array of numbers
@@ -31,7 +6,9 @@ let sum = [];
 
 // 3. Have initial array (Current operand)
 
-// 4. Have baseline sum set to a blank array for all clear
+// 4. Have baseline sum set to a blank array for all clear and answers (do I need 
+// blank sum if I just push innerHTML to output?(sum needed because I can't have 2 arrays that
+// are const but also editable?))
 
 // 5. On num button click add innerHTML to initial array in order of incidence
 
@@ -39,89 +16,97 @@ let sum = [];
 // then passes initial array to second array (previous operand)
 
 // 7. Equals button that when pressed converts initial array to string 
-// and uses operation between the initial and second arrays, passing answer to output array
+// and uses operation between the initial and second arrays, passing answer to second array
 
-// numBtn.forEach(button =>{
-//     button.addEventListener("click" , () => {
-//         Calculator.inputNumber(button.textContent)
-//         Calculator.updateOutput()
-//     })
-// })
+// // if last button operator
 
-// class Calculator {
-//     constructor (currentOp, prevOp){
-//         this.prevOp = prevOp
-//         this.currentOp = currentOp
-//     }
 
-//     clear() {
-//         this.currentOp = ""
-//         this.prevOp = ""
-//         this.operation = undefined
-    
-//     }
-    
-//     delete() {
-    
-//     }
-    
-//     inputNumber(number) {
-//         this.currentOp = number
-//     }
-    
-//     inputOperation(operation) {
-    
-//     }
-    
-//     compute() {
-    
-//     }
-    
-//     updateOutput () {
-//         this.currentOp.innerText = this.currentOp
-//     }
-// }
+const numBtn = document.querySelectorAll(".num__btn");
+const acBtn = document.querySelector("#ac__btn");
+const delBtn = document.querySelector("#del__btn");
+const eqBtn = document.querySelector("#eq__btn");
 
-// numBtn.forEach(btn =>{
-//     btn.addEventListener("click" , () => {
-//         console.log(btn.innerHTML)
-//     })
-// })
+const opBtn = document.querySelectorAll(".op__btn");
+const output = document.querySelectorAll(".output");
+const prevOp = document.querySelector("#prev__Op");
+const currentOp = document.querySelector("#current__Op");
+  
+// const addBtn = document.querySelector("#add__btn").addEventListener("click" , clickOp);
 
-// opBtn.forEach(button =>{
-//     button.addEventListener("click" , () => {
-//         console.log(button)
-//     })
-// })
 
-// acBtn.forEach(button =>{
-//     button.addEventListener("click" , () => {
-//         console.log(button)
-//     })
-// })
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "."];
+const operators = ["+", "-", "/", "*"]
+// let currentLine = currentOp;
+let opUsed = "";
+let sum = "";
 
-// delBtn.forEach(button =>{
-//     button.addEventListener("click" , () => {
-//         console.log(button)
-//     })
-// })
 
-// eqBtn.forEach(button =>{
-//     button.addEventListener("click" , () => {
-//         console.log(button)
-//     })
-// })
-
-// WORKS
-
+// ADD DIGIT PRESSED TO DISPLAY!
 numBtn.forEach(numBtn => {
-    numBtn.addEventListener('click', () => {
+    numBtn.addEventListener("click", () => {
         numbers.forEach(element => {
             if (element == numBtn.dataset.number) {
             currentOp.innerHTML += element;
-            console.log(currentOp.innerHTML);
+            // console.log(currentOp.innerHTML);
             }
             // console.log(numBtn.innerHTML);
         });
     });
 });
+
+// ADD OPERATOR PRESSED TO DISPLAY!
+opBtn.forEach(opBtn => {
+  opBtn.addEventListener("click", () => {
+      operators.forEach(element => {
+          if (element == opBtn.dataset.operation) {
+          prevOp.innerHTML = currentOp.innerHTML;
+          currentOp.innerHTML = element += "";
+          sum = prevOp.innerHTML;
+          }
+      });
+  });
+});
+
+// ALL CLEAR BUTTON!
+acBtn.addEventListener("click", () => {
+    currentOp.innerHTML = "";
+    prevOp.innerHTML = "";
+    output.innerHTML = "";
+});
+
+eqBtn.addEventListener("click", () => {calcAns()})
+
+calcAns = () => {
+  if (currentOp.innerHTML.includes("+")) {
+    const working = currentOp.innerHTML;
+    const recentAns = working.substring(1);
+    currentAns = parseFloat(prevOp.innerHTML) + parseFloat(recentAns);
+    console.log(currentAns);
+    prevOp.innerHTML = currentAns;
+    console.log(prevOp.innerHTML);
+    currentOp.innerHTML = "";
+  } else if (currentOp.innerHTML.includes("-")) {
+    const working = currentOp.innerHTML;
+    const recentAns = working.substring(1);
+    currentAns = parseFloat(prevOp.innerHTML) - parseFloat(recentAns);
+    console.log(currentAns);
+    prevOp.innerHTML = currentAns;
+    console.log(prevOp.innerHTML);
+    currentOp.innerHTML = "";
+  } else if (currentOp.innerHTML.includes("*")) {
+    const working = currentOp.innerHTML;
+    const recentAns = working.substring(1);
+    currentAns = parseFloat(prevOp.innerHTML) * parseFloat(recentAns);
+    console.log(currentAns);
+    prevOp.innerHTML = currentAns;
+    console.log(prevOp.innerHTML);
+    currentOp.innerHTML = "";
+  } else if (currentOp.innerHTML.includes("/")) {
+    const working = currentOp.innerHTML;
+    const recentAns = working.substring(1);
+    currentAns = parseFloat(prevOp.innerHTML) / parseFloat(recentAns);
+    console.log(currentAns);
+    prevOp.innerHTML = currentAns;
+    console.log(prevOp.innerHTML);
+    currentOp.innerHTML = "";
+  }};
